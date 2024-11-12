220efdc7e567331f6da524ccfa5e22125676c1e9
Farrell Rio Fa <92140651+R10F@users.noreply.github.com>
2024-11-12 09:18:34 +0700

    Update Loading.jsx

--- src/components/loaders/Loading.jsx
+++ src/components/loaders/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
     <img src="/assets/loading-bug.gif" alt="Loading..." className="img-fluid w-25 mb-0" />
-    {/* <h5>Loading... Please Wait!</h5> */}
+    <h5>Loading... Please Wait!</h5>
   </div>
 }
 
