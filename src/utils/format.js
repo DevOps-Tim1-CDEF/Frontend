01caad11ce8357bc4fb8ab67c909cc2b357744d1
Farrell Rio Fa <farrellriofa@gmail.com>
2024-12-09 18:53:31 +0700

    change port

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,11 +1,10 @@
 import moment from "moment";
 
 export const baseUrl = "";
-export const mainUrl = "http://localhost:2500/"
+export const mainUrl = "http://localhost:30001/";
 
 export const datetimeFormat = (datetime, format = "DD/MM/YYYY HH:mm") => {
-  return new Date(datetime).getTime()+(2*86400000) < new Date().getTime()
-         ? moment(datetime).format(format)
-         : moment(datetime).fromNow()
-                        
-}
+  return new Date(datetime).getTime() + 2 * 86400000 < new Date().getTime()
+    ? moment(datetime).format(format)
+    : moment(datetime).fromNow();
+};
